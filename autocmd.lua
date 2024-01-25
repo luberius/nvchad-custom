96358248627b89e44903b01f5c328f6e750ad121
syahril <callmesyahril@gmail.com>
2024-01-25 19:39:22 +0700

    v1.3: fix templ format on save

--- autocmd.lua
+++ autocmd.lua
@@ -3,9 +3,32 @@ local augroup = vim.api.nvim_create_augroup
 local autocmd = vim.api.nvim_create_autocmd
 augroup("__formatter__", { clear = true })
 autocmd("BufWritePost", {
-	group = "__formatter__",
-	command = ":FormatWrite",
+  group = "__formatter__",
+  command = ":FormatWrite",
 })
 
--- for templ files
-vim.api.nvim_create_autocmd({ "BufWritePre" }, { pattern = { "*.templ" }, callback = vim.lsp.buf.format })
+-- templ format
+
+local templ_format = function()
+  if vim.bo.filetype == "templ" then
+    local bufnr = vim.api.nvim_get_current_buf()
+    local filename = vim.api.nvim_buf_get_name(bufnr)
+    local cmd = "templ fmt " .. vim.fn.shellescape(filename)
+
+    vim.fn.jobstart(cmd, {
+      on_exit = function()
+        -- Reload the buffer only if it's still the current buffer
+        if vim.api.nvim_get_current_buf() == bufnr then
+          vim.cmd "e!"
+        end
+      end,
+    })
+  else
+    vim.lsp.buf.format()
+  end
+end
+
+autocmd("BufWritePre", {
+  pattern = "*.templ",
+  callback = templ_format,
+})
