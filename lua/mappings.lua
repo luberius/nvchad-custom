e6d938304e2d66b8c73a0989d6ff8a2e075114b1
syahril <callmesyahril@gmail.com>
2025-03-22 22:36:11 +0700

    add floating diagnostic mapping

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -5,16 +5,17 @@ require "nvchad.mappings"
 local map = vim.keymap.set
 
 map("n", "<leader>gg", "<cmd>LazyGit<cr>", { desc = "LazyGit" })
-map("n", "<leader>o",
-  function()
-    if vim.bo.filetype == "NvimTree" or vim.bo.filetype == "qf" then
-      vim.cmd.wincmd "p"
-    else
-      vim.cmd.NvimTreeFocus()
-    end
+map("n", "<leader>o", function()
+  if vim.bo.filetype == "NvimTree" or vim.bo.filetype == "qf" then
+    vim.cmd.wincmd "p"
+  else
+    vim.cmd.NvimTreeFocus()
   end
-, { desc = "Toggle Explorer Focus" })
+end, { desc = "Toggle Explorer Focus" })
 map("n", "gr", "<cmd>Telescope lsp_references<cr>", { desc = "LSP references" })
 map("n", "gd", "<cmd>Telescope lsp_definitions<cr>", { desc = "LSP definitions" })
+map("n", "<leader>lf", function()
+  vim.diagnostic.open_float { border = "rounded" }
+end, { desc = "LSP floating diagnostic" })
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
