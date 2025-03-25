ff268d2bfec8c8db750290cac4a7c8128f697104
syahril <callmesyahril@gmail.com>
2025-03-25 10:51:10 +0700

    add git hunk mapping

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -17,5 +17,11 @@ map("n", "gd", "<cmd>Telescope lsp_definitions<cr>", { desc = "LSP definitions" 
 map("n", "<leader>lf", function()
   vim.diagnostic.open_float { border = "rounded" }
 end, { desc = "LSP floating diagnostic" })
+map("n", "<leader>rh", function()
+  require("gitsigns").reset_hunk()
+end, { desc = "Reset hunk" })
+map("n", "<leader>ph", function()
+  require("gitsigns").preview_hunk()
+end, { desc = "Preview hunk" })
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
